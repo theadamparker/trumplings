ffc8bf269451ad8a9c9f18d6e13f9c31510e5616
Adam Parker <adam@theadamparker.com>
2015-12-17 19:40:33 -0500

    Trumplings bounce on click

--- js/scripts.js
+++ js/scripts.js
@@ -1,25 +1,24 @@
-// on load
 $( document ).ready(function() {
 
- //    $('.trumpling').animate({
-	// 	top: "0%"
-	// }, {
-	// 	duration: 1000,
-	// 	easing: "easeOutBounce"
-	// });
+	$(".trumpling").each(function (i) {
+		var $item = $(this); 
+		setTimeout(function() { 
+		$item.animate({"top": 0}, { 
+			duration: 1000, 
+			easing: "easeOutBounce"});
+		}, 150*i);
+	});
 
-$(".trumpling").each(function (i) {
-    // store the item around for use in the 'timeout' function
-    var $item = $(this); 
-    // execute this function sometime later:
-    setTimeout(function() { 
-      $item.animate({"top": 0}, { duration: 1000, easing: "easeOutBounce"});
-    }, 150*i);
-    // each element should animate half a second after the last one.
-  });
+	function doBounce(element, times, distance, speed) {
+	    for(var i = 0; i < times; i++) {
+	        element.animate({marginTop: '-='+distance}, speed)
+	            .animate({marginTop: '+='+distance}, speed);
+	    }        
+	}	
 
-
-
+	$(".trumpling").click(function(){
+	    doBounce($(this), 2, '10px', 100);
+	});
 });
 
 
