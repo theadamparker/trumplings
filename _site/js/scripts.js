2b4d7d2de4227ec903518c651136db8472d984e3
Adam Parker <adam@theadamparker.com>
2015-12-17 19:55:46 -0500

    Added animated steamer

--- _site/js/scripts.js
+++ _site/js/scripts.js
@@ -1,13 +1,27 @@
 $( document ).ready(function() {
 
-	$(".trumpling").each(function (i) {
-		var $item = $(this); 
-		setTimeout(function() { 
-		$item.animate({"top": 0}, { 
-			duration: 1000, 
-			easing: "easeOutBounce"});
-		}, 150*i);
+    $('.steamer').animate({
+		left: "0%"
+	}, {
+		duration: 1000,
+		easing: "easeOutCubic"
 	});
+
+	// delay for a moment
+    setTimeout(introduceTrumplings, 1500);
+
+    function introduceTrumplings() {
+	    $(".trumpling").each(function (i) {
+			var $item = $(this); 
+			setTimeout(function() { 
+			$item.animate({"top": 0}, { 
+				duration: 1000, 
+				easing: "easeOutBounce"});
+			}, 150*i);
+		});
+	}
+
+	
 
 	function doBounce(element, times, distance, speed) {
 	    for(var i = 0; i < times; i++) {
