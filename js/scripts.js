055dba7df5d3a68972eda038f004e1994b7c21a9
Adam Parker <adam@theadamparker.com>
2016-01-06 12:30:44 -0500

    Cropped trumpling. Removed extraneous files

--- js/scripts.js
+++ js/scripts.js
@@ -11,10 +11,17 @@ $( document ).ready(function() {
     setTimeout(introduceTrumplings, 1500);
 
     function introduceTrumplings() {
+    	var randomMargin = Math.floor(95 + (1-5-95)*Math.random()) + "%" ;
+		var randomWidth = Math.floor(15 + (1+10-15)*Math.random()) + "rem" ;
+		var randomRotation = Math.floor((-45) + (1+45-(-45))*Math.random()) + "deg" ;
+
 		$(".trumpling").box2d({'y-velocity':10});
 
 		$("#addtrumpling").click(function() {
-			$('<img class="trumpling" src="/img/trumpling.png" alt="trumpling" />').appendTo(".steamwrap").box2d({'y-velocity':10});
+			$('<img class="trumpling" src="/img/trumpling.png" alt="trumpling" />')
+			.appendTo(".steamwrap")
+			.css({ left : randomMargin , width : randomWidth , rotate: randomRotation})
+			.box2d({'y-velocity':10});
 		});
 	}
 	
